26e279ea81aada6f9a61526ec23b940aaed238ae
CWJ <wonjon0930@daum.net>
2019-01-19 20:44:02 +0900

    Shuffle Functions created.

--- question/util.go
+++ question/util.go
@@ -73,7 +73,7 @@ func QDetailIdxInit(qdata QData) QData {
 	var qRepIdxIdx int = 0
 	for i := FIRST_IDX; i < len(qdata.RawData.QCWP); i++ {
 		if i == qdata.QRepIdx[qRepIdxIdx] { // if a value to put into restQIdx is same with the value of qRepIdx
-			fmt.Println(qdata.QRepIdx[qRepIdxIdx])
+			//fmt.Println(qdata.QRepIdx[qRepIdxIdx])
 			if qRepIdxIdx+1 != len(qdata.QRepIdx) { // if not qRepIdx[qRepIdxIdx] is the very last value
 				qRepIdxIdx++
 			}
@@ -104,26 +104,44 @@ func QDetailIdxInit(qdata QData) QData {
 }
 
 // 5. shuffle QRepIdx of the structure QData
-func QRepIdxShuffle(qdata QData) QData {
 
-	Copy_QRep := make([]int, len(qdata.QRepIdx))
-	//copy(qrepIdx, qdata.QRepIdx)
-
+func QRepIdxShuffle(qdata QData) QData { //qdata의 qRepIdx를 섞는다.
 	qreplength := len(qdata.QRepIdx)
-	isFilled := make([]bool, qreplength) // 새로 만들 qRep이 채워졌는지 ,모두 false 값
 
 	rand_seed := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(rand_seed)
+	r := rand.New(rand_seed) // To pick SliceIdx Randomly
 
-	n := 0
-	for n < qreplength {
-		if new_qrepIdx := r.Intn(qreplength); !isFilled[new_qrepIdx] {
-			Copy_QRep[n] = qdata.QRepIdx[new_qrepIdx]
-			isFilled[new_qrepIdx] = true
-			n++
+	for i := 0; i < qreplength; i++ { // Using knuth Shuffle
+		idxpicker := r.Intn(qreplength-i) + i
+		// index range : i ~ qreplength-1
+		temp := qdata.QRepIdx[i]
+		qdata.QRepIdx[i] = qdata.QRepIdx[idxpicker]
+		qdata.QRepIdx[idxpicker] = temp
+		// Swap
+	}
+	return qdata
+}
+
+// shuffle QDetIdx of the structure QData
+
+func QDetailIdxShuffle(qdata QData, pattern []int) QData {
+
+	patlength := len(pattern)
+
+	rand_seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(rand_seed) // To pick SliceIdx Randomly
+
+	for i := 0; i < patlength; i++ { // 패턴 슬라이스의 길이만큼 이터레이트
+		qdetlength := len(qdata.QDetailIdx[pattern[i]]) // 패턴 인덱스에 해당하는 질문 슬라이스의 길이
+		for j := 0; j < qdetlength; j++ {               // Using knuth Shuffle
+			idxpicker := r.Intn(qdetlength-j) + j
+			// index range : j ~ qdetlength-1
+			temp := qdata.QDetailIdx[pattern[i]][j]
+			qdata.QDetailIdx[pattern[i]][j] = qdata.QDetailIdx[pattern[i]][idxpicker]
+			qdata.QDetailIdx[pattern[i]][idxpicker] = temp
+			// Swap
 		}
 	}
-	copy(qdata.QRepIdx, Copy_QRep)
 	return qdata
 }
 
@@ -144,3 +162,51 @@ func PrintStruct(qdata QData) {
 	fmt.Println(qdata.QDetailIdx)
 	fmt.Println(qdata.Answer)
 }
+
+/* Old QRepIdxShuffle Algorithm
+func QRepIdxShuffle(qdata QData) QData {
+
+	Copy_QRep := make([]int, len(qdata.QRepIdx))
+
+	qreplength := len(qdata.QRepIdx)
+	isFilled := make([]bool, qreplength) // 새로 만들 qRep이 채워졌는지 ,모두 false 값
+
+	rand_seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(rand_seed)
+
+	n := 0
+	for n < qreplength {
+		if new_qrepIdx := r.Intn(qreplength); !isFilled[new_qrepIdx] {
+			Copy_QRep[n] = qdata.QRepIdx[new_qrepIdx]
+			isFilled[new_qrepIdx] = true
+			n++
+		}
+	}
+	copy(qdata.QRepIdx, Copy_QRep)
+	return qdata
+}
+*/
+/* Old QDetailIdxShuffle Algorithm
+func QDetailIdxShuffle(qdata QData, pattern []int) QData { // int pattern 배열을 받는다.
+	// 대표질문의 컷오프값을 넘긴 변증의 index값들을 담고있다.
+	Copy_QDet := make([][]int, len(qdata.QDetailIdx))
+	copy(Copy_QDet, qdata.QDetailIdx)                  // QDetailIdx 전체를 복사해온다.
+	rand_seed := rand.NewSource(time.Now().UnixNano()) // Random Shuffle을 위함
+	r := rand.New(rand_seed)                           // Random Shuffle을 위함
+
+	for i := 0; i < len(pattern); i++ { // 해당하는 패턴들에 대해 Shuffle 반복 진행
+		qdetlength := len(Copy_QDet[pattern[i]])
+		isFilled := make([]bool, qdetlength)
+		n := 0
+		for n < qdetlength { // 해당하는 패턴의 길이만큼 Shuffle 진
+			if new_qdetIdx := r.Intn(qdetlength); !isFilled[new_qdetIdx] {
+				Copy_QDet[pattern[i]][n] = qdata.QDetailIdx[pattern[i]][new_qdetIdx]
+				isFilled[new_qdetIdx] = true
+				n++
+			}
+		}
+	}
+	copy(qdata.QDetailIdx, Copy_QDet)
+	return qdata
+}
+*/
--- testMain/main.go
+++ testMain/main.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	var qdata question.QData
 
+	p := []int{2, 3, 4} // pattern idx : index range - 0~4(Number of Patterns)
+
 	qdata.RawData = question.LoadData()
 	qdata = question.QIdxInit(qdata)
 	qdata = question.QRepIdxInit(qdata)
+	qdata = question.QDetailIdxInit(qdata)
 	qdata = question.QRepIdxShuffle(qdata)
+	qdata = question.QDetailIdxShuffle(qdata, p)
 	question.PrintStruct(qdata)
-
 }
