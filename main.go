80f35ff3681e5f6c86fab5b7091751aa828a7e6d
GanghoonLee <zxc8594@naver.com>
2019-01-23 16:45:38 +0900

    trimmed for server

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"log"
 	"munzini/handler"
+	"munzini/question" // for test
 	"net/http"
 	"os"
-	"munzini/question"	// for test
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("resources"))
 	http.Handle("/resources/", http.StripPrefix("/resources/", fileServer))
-	http.HandleFunc("/magicball", handler.Dispatch)
+	http.HandleFunc("/munzini", handler.Dispatch)
 	http.HandleFunc("/health_check", handler.HealthCheck)
 	http.HandleFunc("/monitor/l7check", handler.HealthCheck)
 
-	port := os.Getenv("PORT")	// added
-	log.Fatalln(http.ListenAndServe(":" + port, nil))	// updated: ":13780" -> ":" + port
+	port := os.Getenv("PORT")                       // added
+	log.Fatalln(http.ListenAndServe(":"+port, nil)) // updated: ":13780" -> ":" + port
 }
