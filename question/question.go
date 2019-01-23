f83d2750c2d0352ff86f3c2494182aacbe76b9a2
GanghoonLee <zxc8594@naver.com>
2019-01-23 16:28:00 +0900

    calculateFinalScore, PrepareFin created

--- question/question.go
+++ question/question.go
@@ -1,11 +1,13 @@
 // question
 package question
 
-const PATTERN_NUM = 5 // 패턴 수
-const PATTEN_NAME = {"칠정", "노권", "담음", "식적", "어혈"}
+const PATTERN_NUM = 5 // 변증 
+const PATTERN_NAME = {"칠정", "노권", "담음", "식적", "어혈"}	// 변증 이름
+const PATTERN_INDEX = {"칠정" : 0, "노권" : 1, "담음" : 2, "식적" : 3, "어혈" : 4}	// 변증 인덱스 : 이름
 const BI_CRITERIA = 3 // 이분화 기준
+const SCORE_MAX = 5	// 점수 최댓값
 
-const FIRST_IDX = 1 // QCWP 실제 데이터 시 행번호
+const FIRST_IDX = 1 // QCWP 실제 데이터 시작 행번호
 const QUESTION = 0  // QCWP question 열번호
 const CATEGORY = 1  // QCWP category 열번호
 const WEIGHT = 2    // QCWP weight 열번호
@@ -25,6 +27,5 @@ type QData struct {
 	QDetailIdx        [][]int     // [칠정에 대한 QCWP 인덱스 슬라이스, 노권에 대한 QCWP 인덱스 슬라이스, ..., 어혈에 대한 QCWP 인덱스 슬라이스]
 	Answer            map[int]int // QCWP 인덱스 : 응답점수
 	SQSProbPatternIdx []int       // 간단한 문진 이후 컷오프 값을 넘긴 Pattern의 인덱스 슬라이스
-	DQSProbPatternIdx []int       // 최종 문진 이후 컷오프 값을 넘긴 Pattern의 인덱스 슬라이스
-	FinalScore	[][]int	// 간단한 문진 이후 컷오프 값을 넘긴 Pattern에 대한 표준점수
+	FinalScore	[]float32	// 간단한 문진 이후 컷오프 값을 넘긴 Pattern 인덱스 에 대한 표준점수 슬라이스
 }
