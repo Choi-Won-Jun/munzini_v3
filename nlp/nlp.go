9af3cd02d071dce5e76b17e4865bca9f053f634e
GanghoonLee <zxc8594@naver.com>
2019-01-24 14:52:20 +0900

    package nlp created

---
+++ nlp/nlp.go
@@ -0,0 +1,18 @@
+package nlp
+
+func ConvertInquiryScore(str string) string {
+	switch str {
+	case "일":
+		str = "1"
+	case "이":
+		str = "2"
+	case "삼":
+		str = "3"
+	case "사":
+		str = "4"
+	case "오":
+		str = "5"
+	}
+
+	return str
+}
