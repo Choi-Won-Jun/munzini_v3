31ea5cf340292de924df079d32394d0f1f17aca0
GanghoonLee <zxc8594@naver.com>
2019-01-29 13:44:57 +0900

    nlp updated

--- nlp/nlp.go
+++ nlp/nlp.go
@@ -5,7 +5,7 @@ import "strconv"
 var exStrOne = []string{"일", "일본", "일정"}
 var exStrTwo = []string{"이", "이정"}
 var exStrThr = []string{"삼", "상점", "암점", "삼원", "한번"}
-var exStrFor = []string{"사", "사정", "4동", "서점", "서번", "사본", "사전", "화정", "화성", "다정", "아점", "상가점", "카본"}
+var exStrFor = []string{"사", "사정", "4동", "서점", "서번", "사본", "사전", "화정", "화성", "다정", "아점", "상가점", "카본", "서본"}
 var exStrFif = []string{"오", "오정", "호점", "오전"}
 
 var exStrArr = [][]string{exStrOne, exStrTwo, exStrThr, exStrFor, exStrFif}
