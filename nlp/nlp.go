f2dbbbf8a1010c8ba430678c1b4854b342edc688
CWJ <wonjon0930@daum.net>
2019-02-19 16:46:00 +0900

    Exception(이점) handling.

--- nlp/nlp.go
+++ nlp/nlp.go
@@ -2,10 +2,10 @@ package nlp
 
 // ConvertInquiryScore 관련 구조체
 var exStrOne = []string{"일", "일본", "일정"}                                                                    // 1점에 관한 예외 단어
-var exStrTwo = []string{"이", "이정"}                                                                          // 2점에 관한 예외 단어
+var exStrTwo = []string{"이", "이정", "이점"}                                                                    // 2점에 관한 예외 단어
 var exStrThr = []string{"삼", "상점", "암점", "삼원", "한번"}                                                        // 3점에 관한 예외 단어
 var exStrFor = []string{"사", "사정", "4동", "서점", "서번", "사본", "사전", "화정", "화성", "다정", "아점", "상가점", "카본", "서본"} // 4점에 관한 예외 단어
-var exStrFif = []string{"오", "오정", "호점", "오전"}                                                              // 5점에 관한 예외 단
+var exStrFif = []string{"오", "오정", "호점", "오전"}                                                              // 5점에 관한 예외 단어
 
 var exStrArr = [][]string{exStrOne, exStrTwo, exStrThr, exStrFor, exStrFif}
 
