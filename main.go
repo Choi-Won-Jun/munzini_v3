2d8b4eb5bb5df0c7f2b4f6b84b1d11487b8f8abd
Hojun Lim <lhj940825@gmail.com>
2019-03-07 14:15:54 +0900

    Revision Paths in Packages,Munzinis->Munzinis_project

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"log"
-	"munzini/handler"
+	"munzinis_project/handler"
 	"net/http"
 	"os"
 )
 
+// Imported Packages only for Local Test
+// import (
+// 	"fmt"
+// 	"log"
+// 	"munzinis_project/handler"
+// 	"net/http"
+// )
+
 func main() {
 	fileServer := http.FileServer(http.Dir("resources"))
+
 	http.Handle("/resources/", http.StripPrefix("/resources/", fileServer))
 	http.HandleFunc("/munzini", handler.Dispatch)
 	http.HandleFunc("/health_check", handler.HealthCheck)
