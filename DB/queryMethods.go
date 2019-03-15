3aacee666e8c6d5b27e35972dd89dd74bc96845c
Hojun Lim <lhj940825@gmail.com>
2019-03-15 16:32:53 +0900

    123

--- DB/queryMethods.go
+++ DB/queryMethods.go
@@ -164,7 +164,7 @@ func RetreiveRecentMedicalRecordByUserID(userID string) []MedicalRecord {
 
 	}
 
-	return medicalRecords[len(medicalRecords)-NUM_MR_to_CHECK : len(medicalRecords)-1]
+	return medicalRecords[len(medicalRecords)-NUM_MR_to_CHECK : len(medicalRecords)]
 
 }
 
